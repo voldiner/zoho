3adfd8a95e02c259802331f46c3cb8e854fed0de
voldiner <yura.voldiner@gmail.com>
2021-12-08 22:04:13 +0200

    create getRecord method

--- app/Http/Controllers/DealController.php
+++ app/Http/Controllers/DealController.php
@@ -3,25 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Env;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 class DealController extends Controller
 {
     public function create()
     {
-        //dd($this->refreshToken());
-        // test for api
+        dump(session()->all());
+
         $url = 'https://www.zohoapis.com/crm/v2/Deals';
-        $token = '1000.ab03d356975efa9fa1eb8b8a6013a814.2449f9ca7e694bfa0ba6e9b60098946d';
+        $token = $this->getToken();
         //$response = Http::withToken($token)->get($url);
         //dd($response->object()->status); // 'error'
         //dd($response->object()->data[0]->id); // 'error'
         // ---- create deal
         $array_param[] = [
-            'Owner' => ["id" => "5149635000000345001"],
-            "Account_Name" => ["id" => "5149635000000354785"],
-            "Contact_Name" => ["id" => "5149635000000354878"],
-            "Campaign_Source" => ["id" => "5149635000000382002"],
+            'Owner' => ["id" => $this->getUser()->id],
+            "Account_Name" => ["id" => $this->getRecord('Accounts')->id],
+            "Contact_Name" => ["id" => $this->getRecord('Contacts')->id],
+            "Campaign_Source" => ["id" => $this->getRecord('Campaigns')->id],
             "Type" => "New Business",
             "Description" => "Design your own layouts that align your business processes precisely. Assign them to profiles appropriately.",
             "Deal_Name" => "Deal_Name_Voldiner",
@@ -36,7 +38,7 @@ class DealController extends Controller
 
         $response = Http::withToken($token)
             ->acceptJson()
-            ->post($url, $parameters);
+           ->post($url, $parameters);
         dd($response->object());
 
     }
@@ -46,12 +48,69 @@ class DealController extends Controller
         $url = 'https://accounts.zoho.com/oauth/v2/token';
 
         $response = Http::asForm()->post($url, [
-            'client_id' => '1000.JW2BLTG0XC0NCYKZ8EXIJHGH4FYFCO',
-            'client_secret' => '865d86fd87971686378cee5d5730058b1f1a607414',
-            'refresh_token' => '1000.ea8830eef03958a61701d0113f46ed75.96627cc55d6457f66aa915c09022558d',
+            'client_id' => env('CLIENT_ID'),
+            'client_secret' => env('CLIENT_SECRET'),
+            'refresh_token' => env('REFRESH_TOKEN'),
             'grant_type' => 'refresh_token',
         ]);
-        return $response->object()->access_token;
-
+        $token = $response->object()->access_token;
+        session()->put('token', $token);
+        return $token;
     }
+
+    /**
+     *
+     * отримати першого користувача для створення deal
+     */
+    public function getUser()
+    {
+        $url = 'https://www.zohoapis.com/crm/v2/users';
+        $token = $this->getToken();
+        $response = Http::withToken($token)
+            ->acceptJson()
+            ->get($url);
+
+        if (isset($response->object()->users[0])) {
+            $user = $response->object()->users[0];
+            session()->put('user_id', $user->id);
+            return $user;
+        }
+        return false;
+    }
+
+    /**
+     * @param string $name
+     * отримати першу запис для створення deal
+     * @return object|false
+     */
+    public function getRecord($name)
+    {
+        $url = 'https://www.zohoapis.com/crm/v2/' . $name;
+        $token = $this->getToken();
+        $response = Http::withToken($token)
+            ->acceptJson()
+            ->get($url);
+
+        if (isset($response->object()->data[0])) {
+            $item = $response->object()->data[0];
+            $key = Str::lower($name) . '_id';
+            session([ $key => $item->id]);
+            return $item;
+        }
+        return false;
+    }
+
+
+    /**
+     * @return string $result
+     */
+    public function getToken()
+    {
+        if (!session()->has('token')) {
+            $this->refreshToken();
+        }
+        $result = session('token');
+        return $result;
+    }
+
 }
